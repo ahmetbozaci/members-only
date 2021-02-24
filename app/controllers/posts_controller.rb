5769c371d30264790dd169c847b8c6a3f427f0c1
chrystalme <afam_ifeanyi@live.com>
2021-02-24 17:41:30 +0200

    Create views and method create in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,26 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!, only: %i[new create]
 
+  def index
+    @post = Post.all
+  end
+
   def new
     @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
   end
 end
