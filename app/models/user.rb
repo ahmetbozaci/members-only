68d5e17373855dde1b02515130d19c03a297ca1d
Ahmet Bozaci <ahmt9417@gmail.com>
2021-02-25 12:49:07 +0300

    Added validation to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
 
-  validates :name, presence: true # just added after push
+  validates :name, presence: true, uniqueness: true # just added after push
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
