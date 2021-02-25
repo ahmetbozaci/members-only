55027fa97d636f8669cbb267006ccede4c2ce698
chrystalme <afam_ifeanyi@live.com>
2021-02-25 11:38:12 +0200

    Add Sign Out action and logic

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts, only: %i[new index create]
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
