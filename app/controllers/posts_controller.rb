b12c6d48a64a68cdc63f3c418c39a67f4c36ceb3
chrystalme <afam_ifeanyi@live.com>
2021-02-25 15:11:26 +0200

    Modified application.html.erb to show sign_in and sign_out

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC')
     @post = Post.new
   end
 
