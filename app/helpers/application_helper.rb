48001374d46eb33baf7f74de3999e4187c87db4e
chrystalme <afam_ifeanyi@live.com>
2021-02-26 12:45:44 +0200

    Moved logic to helper for sign_in and sign_out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def view_sign_in
+    if user_signed_in?
+      "<p> #{current_user.name } |
+       #{link_to 'Sign Out', destroy_user_session_path} </p>".html_safe
+    else
+      "<p>
+        #{link_to 'Sign in', new_user_session_path}
+        </p>".html_safe
+    end
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,21 @@ module PostsHelper
       "User"
     end
   end
+
+  def author(posts)
+    user_signed_in? ? posts.title : "Anonymous"
+  end
+
+  def show_author(user = "Anonymous") 
+    if user_signed_in?
+      "<p> Author name:
+          #{ user.name }
+       </p>".html_safe
+    else
+      "<p> Author name:
+       #{user.name}
+      </p>".html_safe  
+    end
+  end
 end
+
